5837dbd4059b6b743543ed214469d7ac7ee424cf
urkskur <urkskurth@gmail.com>
2020-02-04 22:37:32 -0800

    dog_days

--- sketch.js
+++ sketch.js
@@ -1,7 +1,33 @@
 function setup() {
-  // put setup code here
+  createCanvas(500, 500, WEBGL);
+
+  let cycle = 29;
+
+
+  let detailX;
+  detailX = createSlider(3, 24, 3);
+  detailX.position(10, height + 5);
+  detailX.style('width', '80px');
+
+   detailY = createSlider(3, 16, 3);
+  detailY.position(10, height + 5);
+  detailY.style('width', '80px');
+
 }
 
 function draw() {
-  // put drawing code here
+	background(55);
+	rotateX(frameCount * 0.01);
+	rotateY(frameCount * 0.01);
+  ellipse(50, 50, 100);
+
+plane(45, 75);
+
+torus(55, 49, detailX.value, detailY.value);
+
+
+}
+
+function drawMoon(moonphase, moonsize){
+	
 }
